7e9942cd586ddc971c059f8d4c99f7ba0c830020
Alexandros <alexgrig1@hotmail.com>
2023-05-08 19:03:29 +0300

    first code

--- Functions.py
+++ Functions.py
@@ -1,11 +1,11 @@
-import sqlite3
 import tkinter as tk
+import sqlite3
 from tkinter import messagebox
 import psutil
 import ctypes
+import pystray
+from PIL import Image
 
-
-# Constants for changing the mouse speed
 SPI_SETMOUSESPEED = 0x0071
 SPIF_UPDATEINIFILE = 0x01
 SPIF_SENDCHANGE = 0x02
@@ -14,9 +14,7 @@ SPIF_SENDCHANGE = 0x02
 def check_credentials(username, password):
     conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
-
     cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
-
     result = cursor.fetchone()
     conn.close()
 
@@ -26,66 +24,144 @@ def check_credentials(username, password):
         return None
 
 
-
 def login(username_entry, password_entry, app):
     username = username_entry.get()
     password = password_entry.get()
-
     result = check_credentials(username, password)
 
     if result:
         user_id, time_remaining = result
         messagebox.showinfo("Login successful", f"Welcome, {username}!")
-        app.destroy()  # Close the login window
-        show_main_window(user_id, time_remaining)
+        show_main_window(user_id, time_remaining, app)
     else:
         messagebox.showerror("Login failed", "Incorrect username or password")
 
-# Function to change the mouse speed
+
 def set_mouse_speed(speed):
     user32 = ctypes.windll.user32
     user32.SystemParametersInfoW(SPI_SETMOUSESPEED, 0, speed, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE)
 
 
-# Function to handle changes to the mouse speed slider
 def on_mouse_speed_change(value):
     set_mouse_speed(int(value))
 
 
-def show_main_window(user_id, time_remaining):
-    main_app = tk.Tk()
-    main_app.title("Main App")
+def create_system_tray_icon(icon_image_path, widget_manager_window, app):
+    def show_widget_manager(icon, item):
+        icon.stop()
+        widget_manager_window.deiconify()
+        app.withdraw()
+
+    image = Image.open(icon_image_path)
+    icon = pystray.Icon("widget_manager")
+    icon.menu = pystray.Menu(pystray.MenuItem('Show Widget Manager', show_widget_manager),
+                             pystray.MenuItem('Exit', app.quit))
+    icon.icon = image
+    icon.run()
 
 
+def on_window_drag(event, window):
+    x, y = event.x_root, event.y_root
+    window.geometry(f"+{x}+{y}")
 
-    # Create the mouse speed slider
-    mouse_speed_label = tk.Label(main_app, text="Mouse Speed")
+
+def show_main_window(user_id, time_remaining, app):
+    app.withdraw()  # Hide the login window
+
+    def do_nothing():
+        pass
+
+    app.protocol("WM_DELETE_WINDOW", do_nothing)  # Prevent closing with 'x' button
+
+    def show_login_window():
+        mouse_sensitivity_window.destroy()
+        hardware_usage_window.destroy()
+        widget_manager_window.destroy()
+        app.deiconify()  # Show the login window
+
+    def close_widget_manager_window():
+        create_system_tray_icon('V:\Alexandros\sxolh\icon.png', widget_manager_window, app)
+
+    # Mouse Sensitivity window
+    mouse_sensitivity_window = tk.Toplevel()
+    mouse_sensitivity_window.title("Mouse Sensitivity")
+    mouse_sensitivity_window.withdraw()  # Hide the window initially
+    mouse_sensitivity_window.protocol("WM_DELETE_WINDOW", do_nothing)  # Prevent closing with 'x' button
+    mouse_sensitivity_window.overrideredirect(True)  # Remove window decorations and taskbar entry
+
+    # Create a draggable frame inside the Mouse Sensitivity window
+    draggable_frame = tk.Frame(mouse_sensitivity_window)
+    draggable_frame.pack()
+    draggable_frame.bind('<B1-Motion>', lambda event: on_window_drag(event, mouse_sensitivity_window))
+
+    mouse_speed_label = tk.Label(draggable_frame, text="Mouse Speed")
     mouse_speed_label.pack()
-    mouse_speed_slider = tk.Scale(main_app, from_=1, to=20, orient="horizontal", length=200)
+    mouse_speed_slider = tk.Scale(draggable_frame, from_=1, to=20, orient="horizontal", length=200,
+                                  command=on_mouse_speed_change)
     mouse_speed_slider.set(10)
     mouse_speed_slider.pack()
 
-    mouse_speed_slider.config(command=on_mouse_speed_change)
+    # Hardware Usage window
+    hardware_usage_window = tk.Toplevel()
+    hardware_usage_window.title("Hardware Usage")
+    hardware_usage_window.withdraw()  # Hide the window initially
+    hardware_usage_window.protocol("WM_DELETE_WINDOW", do_nothing)  # Prevent closing with 'x' button
+    hardware_usage_window.overrideredirect(True)  # Remove window decorations and taskbar entry
 
     time_remaining_var = tk.StringVar()
     time_remaining_var.set(f"Time remaining: {time_remaining} seconds")
-
-    time_remaining_label = tk.Label(main_app, textvariable=time_remaining_var)
+    time_remaining_label = tk.Label(hardware_usage_window, textvariable=time_remaining_var)
     time_remaining_label.pack()
 
     cpu_ram_usage_var = tk.StringVar()
-
-    cpu_ram_usage_label = tk.Label(main_app, textvariable=cpu_ram_usage_var)
+    cpu_ram_usage_label = tk.Label(hardware_usage_window, textvariable=cpu_ram_usage_var)
     cpu_ram_usage_label.pack()
+
+    # Make the Hardware Usage window draggable
+    hardware_usage_window.bind('<B1-Motion>', lambda event: on_window_drag(event, hardware_usage_window))
 
     def update_cpu_ram_usage():
         cpu_percent = psutil.cpu_percent()
         ram_percent = psutil.virtual_memory().percent
-
         cpu_ram_usage_var.set(f"CPU: {cpu_percent}% | RAM: {ram_percent}%")
-
-        main_app.after(1000, update_cpu_ram_usage)
+        hardware_usage_window.after(1000, update_cpu_ram_usage)
 
     update_cpu_ram_usage()
 
-    main_app.mainloop()
+    # Widget Manager window
+    widget_manager_window = tk.Toplevel()
+    widget_manager_window.title("Widget Manager")
+    widget_manager_window.protocol("WM_DELETE_WINDOW", close_widget_manager_window)
+
+    hardware_usage_var = tk.BooleanVar()
+    mouse_sensitivity_var = tk.BooleanVar()
+
+    def toggle_hardware_usage_window():
+        if hardware_usage_var.get():
+            hardware_usage_window.deiconify()
+        else:
+            hardware_usage_window.withdraw()
+
+    def toggle_mouse_sensitivity_window():
+        if mouse_sensitivity_var.get():
+            mouse_sensitivity_window.deiconify()
+        else:
+            mouse_sensitivity_window.withdraw()
+
+    hardware_usage_checkbutton = tk.Checkbutton(widget_manager_window, text="Hardware Usage",
+                                                variable=hardware_usage_var,
+                                                command=toggle_hardware_usage_window)
+    hardware_usage_checkbutton.grid(row=0, column=0)
+
+    mouse_sensitivity_checkbutton = tk.Checkbutton(widget_manager_window, text="Mouse Sensitivity",
+                                                   variable=mouse_sensitivity_var,
+                                                   command=toggle_mouse_sensitivity_window)
+    mouse_sensitivity_checkbutton.grid(row=1, column=0)
+
+    logout_button = tk.Button(widget_manager_window, text="Logout", command=show_login_window)
+    logout_button.grid(row=3, column=0)
+
+    # Close all widget windows when the main application is closed
+    app.protocol("WM_DELETE_WINDOW", app.quit)
+
+    app.mainloop()
--- main.py
+++ main.py
@@ -7,23 +7,23 @@ from Functions import *
 app = tk.Tk()
 app.title("Login App")
 
+app.protocol('WM_DELETE_WINDOW', lambda: None)  #
+
 # Create labels, entries, and radio buttons
 username_label = tk.Label(app, text="Username:")
 username_entry = tk.Entry(app)
 password_label = tk.Label(app, text="Password:")
 password_entry = tk.Entry(app, show="*")
-user_type_var = tk.StringVar()
 login_button = tk.Button(app, text="Login", command=lambda: login(username_entry, password_entry, app))
+
 # Position the elements using grid layout
 username_label.grid(row=0, column=0)
 username_entry.grid(row=0, column=1)
 password_label.grid(row=1, column=0)
 password_entry.grid(row=1, column=1)
-
 login_button.grid(row=3, column=1, sticky="E")
 
+
 # Start the main event loop
+app.protocol("WM_DELETE_WINDOW", lambda: None)  # Disable the close button
 app.mainloop()
-
-
-
